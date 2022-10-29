913444eecf12c4007e8a4e31801e922f78724f05
David Wang <devtesting404@gmail.com>
2022-10-30 00:32:49 +0800

    Reduce minimap scale factor to 4

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -14,14 +14,13 @@
 
 # define WIN_WIDTH 1366
 # define WIN_HEIGHT 768
-# define MINIMAP_SCALE 6
+# define MINIMAP_SCALE 4
 
 # define WHITE 0x00FFFFFF
 # define BLACK 0x00000000
 # define RED 0x00FF0000
 # define GREEN 0x0000FF00
 # define BLUE 0x000000FF
-
 
 enum e_textures{
 	NORTH = 0,
