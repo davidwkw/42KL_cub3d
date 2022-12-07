ed28494b44fb70976f7fb786f7058070e6fbead8
David Wang <devtesting404@gmail.com>
2022-12-07 10:13:53 +0800

    Add player FOV and movement controls

--- src/player_utils.c
+++ src/player_utils.c
@@ -1,28 +1,28 @@
 #include "cub3d.h"
 
 // to refactor so that it takes into account player vector x/y rather than char.
-static void set_camera_plane(char dir, t_vector *cam_vector, double fov)
+static void set_camera_plane(char dir, t_vector *cam_vect, double fov)
 {
 	fov /= 2;
 	if (dir == 'E')
 	{	
-		cam_vector->x = 0;
-		cam_vector->y = tan(fov/180.0*M_PI);
+		cam_vect->x = 0;
+		cam_vect->y = tan(fov/180.0*M_PI);
 	}
 	else if (dir == 'N')
 	{
-		cam_vector->x = tan(fov/180.0*M_PI);
-		cam_vector->y = 0;
+		cam_vect->x = tan(fov/180.0*M_PI);
+		cam_vect->y = 0;
 	}
 	else if (dir == 'W')
 	{
-		cam_vector->x = 0;
-		cam_vector->y = -tan(fov/180.0*M_PI);
+		cam_vect->x = 0;
+		cam_vect->y = -tan(fov/180.0*M_PI);
 	}
 	else if (dir == 'S')
 	{
-		cam_vector->x = -tan(fov/180.0*M_PI);
-		cam_vector->y = 0;
+		cam_vect->x = -tan(fov/180.0*M_PI);
+		cam_vect->y = 0;
 	}
 }
 
@@ -67,37 +67,58 @@ void	init_player_var(t_player *player, char orientation, int x, int y)
 {
 	player->px = x;
 	player->py = y;
-	set_player_dir_vect(orientation, &player->dir_vector);
-	set_camera_plane(orientation, &player->cam_vector, FOV);
+	set_player_dir_vect(orientation, &player->dir_vect);
+	set_camera_plane(orientation, &player->cam_vect, FOV);
 }
 
 static void handle_forw_back(char key, t_player *p, t_map map)
 {
 	if (key == 'w' || key == 13)
 	{
-		if (map.map[(int)(p->py + p->dir_vector.y * MOVE_SPEED)][(int)p->py] != '1')
-			p->py += p->dir_vector.y * MOVE_SPEED;
-		if (map.map[(int)p->py][(int)(p->px  + p->dir_vector.x * MOVE_SPEED)] != '1')
-			p->px += p->dir_vector.x * MOVE_SPEED;
+		if (map.map[(int)(p->py + p->dir_vect.y * MOVE_SPEED)][(int)p->px] != '1')
+			p->py += p->dir_vect.y * MOVE_SPEED;
+		if (map.map[(int)p->py][(int)(p->px  + p->dir_vect.x * MOVE_SPEED)] != '1')
+			p->px += p->dir_vect.x * MOVE_SPEED;
 	}
 	else if (key == 's' || key == 1)
 	{
-		if (map.map[(int)(p->py - p->dir_vector.y * MOVE_SPEED)][(int)p->py] != '1')
-			p->py -= p->dir_vector.y * MOVE_SPEED;
-		if (map.map[(int)p->py][(int)(p->px  - p->dir_vector.x * MOVE_SPEED)] != '1')
-			p->px -= p->dir_vector.x * MOVE_SPEED;
+		if (map.map[(int)(p->py - p->dir_vect.y * MOVE_SPEED)][(int)p->px] != '1')
+			p->py -= p->dir_vect.y * MOVE_SPEED;
+		if (map.map[(int)p->py][(int)(p->px  - p->dir_vect.x * MOVE_SPEED)] != '1')
+			p->px -= p->dir_vect.x * MOVE_SPEED;
 	}
 }
 
 static void	handle_rotation(char key, t_player *p)
 {
+	double	prev_dir_x;
+	double	prev_plane_x;
+
 	if (key == 'a' || key == 0)
 	{
-
+		prev_dir_x = p->dir_vect.x;
+		p->dir_vect.x = p->dir_vect.x * cos(-ROT_SPEED)
+						- p->dir_vect.y * sin(-ROT_SPEED);
+		p->dir_vect.y = prev_dir_x * sin(-ROT_SPEED)
+						+ p->dir_vect.y * cos(-ROT_SPEED);
+		prev_plane_x = p->cam_vect.x;
+		p->cam_vect.x = p->cam_vect.x * cos(-ROT_SPEED)
+						- p->cam_vect.y * sin(-ROT_SPEED);
+		p->cam_vect.y = prev_plane_x * sin(-ROT_SPEED)
+						+ p->cam_vect.y * cos(-ROT_SPEED);
 	}
 	else if (key == 'd' || key == 2)
 	{
-
+		prev_dir_x = p->dir_vect.x;
+		p->dir_vect.x = p->dir_vect.x * cos(ROT_SPEED)
+						- p->dir_vect.y * sin(ROT_SPEED);
+		p->dir_vect.y = prev_dir_x * sin(ROT_SPEED)
+						+ p->dir_vect.y * cos(ROT_SPEED);
+		prev_plane_x = p->cam_vect.x;
+		p->cam_vect.x = p->cam_vect.x * cos(ROT_SPEED)
+						- p->cam_vect.y * sin(ROT_SPEED);
+		p->cam_vect.y = prev_plane_x * sin(ROT_SPEED)
+						+ p->cam_vect.y * cos(ROT_SPEED);
 	}
 }
 
@@ -114,8 +135,8 @@ Returns nothing.
 */
 void	handle_player_movement(int key, t_player *p, t_map map)
 {
-	if (ft_strchr("ws", key) == 0 || key == 13 || key == 1)
+	if (ft_strchr("ws", key) != 0 || key == 13 || key == 1)
 		handle_forw_back(key, p, map);
-	else if (ft_strchr("ad", key) == 0 || key == 0 || key == 2)
+	else if (ft_strchr("ad", key) != 0 || key == 0 || key == 2)
 		handle_rotation(key, p);
 }
