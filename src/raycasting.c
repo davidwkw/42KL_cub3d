8d6efd3bba1567b82944a7da09372bf23d41df1e
David Wang <devtesting404@gmail.com>
2022-12-08 16:16:52 +0800

    Debug raycasting dda algo

--- src/raycasting.c
+++ src/raycasting.c
@@ -15,13 +15,13 @@ static void draw_ver_line(t_data *scrn_data, int x, int start, int end, int colo
 static void	calc_delta(t_ray_vars *ray_vars)
 {
 	if (ray_vars->ray_dir.x == 0)
-		ray_vars->delta_x = 1.7e+308;
+		ray_vars->delta_x = 1e30;
 	else
-		ray_vars->delta_x = ft_absolute(1/ray_vars->ray_dir.x);
+		ray_vars->delta_x = absolute_double(1/ray_vars->ray_dir.x);
 	if (ray_vars->ray_dir.y == 0)
-		ray_vars->delta_y = 1.7e+308;
+		ray_vars->delta_y = 1e30;
 	else
-		ray_vars->delta_y = ft_absolute(1/ray_vars->ray_dir.y);
+		ray_vars->delta_y = absolute_double(1/ray_vars->ray_dir.y);
 }
 
 static void	calc_side_dist(t_ray_vars *ray_vars, t_player p)
@@ -48,12 +48,22 @@ static void	calc_side_dist(t_ray_vars *ray_vars, t_player p)
 	}
 }
 
-static int	determine_side_hit(int val, int a, int b)
+static int	determine_side_hit(t_ray_vars ray_vars)
 {
-	if (val < 0)
-		return a;
+	if (ray_vars.side == 0)
+	{
+		if (ray_vars.ray_dir.x < 0)
+			return (WEST);
+		else
+			return (EAST);
+	}
 	else
-		return b;
+	{
+		if (ray_vars.ray_dir.y < 0)
+			return (NORTH);
+		else
+			return (SOUTH);
+	}
 }
 
 static void	perform_dda(t_ray_vars *ray_vars, char **map)
@@ -67,18 +77,18 @@ static void	perform_dda(t_ray_vars *ray_vars, char **map)
 		{
 			ray_vars->side_x += ray_vars->delta_x;
 			ray_vars->map_x += ray_vars->step_x;
-			ray_vars->side = determine_side_hit(ray_vars->step_x, WEST, EAST);
+			ray_vars->side = 0;
 		}
 		else
 		{
 			ray_vars->side_y += ray_vars->delta_y;
 			ray_vars->map_y += ray_vars->step_y;
-			ray_vars->side = determine_side_hit(ray_vars->step_x, NORTH, SOUTH);
+			ray_vars->side = 1;
 		}
 		if (map[ray_vars->map_y][ray_vars->map_x] == '1')
 			hit = 1;
 	}
-	if (ray_vars->side == EAST || ray_vars->side == WEST)
+	if (ray_vars->side == 0)
 		ray_vars->perp_wall_dist = ray_vars->side_x - ray_vars->delta_x;
 	else
 		ray_vars->perp_wall_dist = ray_vars->side_y - ray_vars->delta_y;
@@ -88,6 +98,7 @@ t_line_vars	calc_line_vals(t_ray_vars ray_vars)
 {
 	t_line_vars	line_vars;
 
+	line_vars = (t_line_vars){};
 	line_vars.line_height = (int)(WIN_HEIGHT / ray_vars.perp_wall_dist);
 	line_vars.line_start = -line_vars.line_height / 2 + WIN_HEIGHT / 2;
 	if (line_vars.line_start < 0)
@@ -103,24 +114,21 @@ int	calc_texture_x(t_ray_vars ray_vars, t_player p, int tex_width)
 	double	wall_x;
 	int		tex_x;
 
-	if (ray_vars.side == WEST || ray_vars.side == EAST)
-		wall_x = p.py + ray_vars.perp_wall_dist * p.dir_vect.y;
+	if (ray_vars.side == 0)
+		wall_x = p.py + ray_vars.perp_wall_dist * ray_vars.ray_dir.y;
 	else
-		wall_x = p.px + ray_vars.perp_wall_dist * p.dir_vect.x;
-	wall_x -= floor(wall_x);
+		wall_x = p.px + ray_vars.perp_wall_dist * ray_vars.ray_dir.x;
+	wall_x -= floorf((wall_x));
 	tex_x = (int)(wall_x * (double)tex_width);
-	if ((ray_vars.side == EAST || ray_vars.side == WEST)
-		&& ray_vars.ray_dir.x > 0)
+	if (ray_vars.side == 0 && ray_vars.ray_dir.x > 0)
 		tex_x = tex_width - tex_x - 1;
-	if ((ray_vars.side == NORTH || ray_vars.side == SOUTH)
-		&& ray_vars.ray_dir.y < 0)
+	if (ray_vars.side == 1 && ray_vars.ray_dir.y < 0)
 		tex_x = tex_width - tex_x - 1;		
 	return tex_x;
 }
 
-
 void draw_textured_line(t_ray_vars ray_vars, t_player p,
-						t_data tex, t_data *scrn_buff)
+						t_data tex_data, t_data *scrn_buff)
 {
 	t_line_vars	line_vars;
 	int			tex_x;
@@ -130,23 +138,34 @@ void draw_textured_line(t_ray_vars ray_vars, t_player p,
 	int			y;
 	int			pixel;
 	int			*scrn;
-	int			*tex_addr;
+	int			*tex;
 
 	scrn = (int *)scrn_buff->addr;
-	tex_addr = (int *)tex.addr; 
+	tex = (int *)tex_data.addr; 
 	line_vars = calc_line_vals(ray_vars);
-	tex_x = calc_texture_x(ray_vars, p, tex.width);
-	step = 1.0 * tex.height / line_vars.line_height;
+	tex_x = calc_texture_x(ray_vars, p, tex_data.width);
+	step = 1.0 * tex_data.height / line_vars.line_height;
 	texture_pos = (line_vars.line_start
 				- WIN_HEIGHT / 2 + line_vars.line_height / 2) * step;
-	y = line_vars.line_start - 1;
-	while (++y < line_vars.line_end)
+	y = line_vars.line_start;
+	while (y < line_vars.line_end)
 	{
-		tex_y = (int)texture_pos & (tex.height - 1);
+		tex_y = (int)texture_pos & (tex_data.height - 1);
 		texture_pos += step;
-		pixel = tex_addr[tex_y * tex.size_line / (tex.bpp / 8) + tex_x];
+		pixel = tex[tex_y * tex_data.size_line / (tex_data.bpp / 8) + tex_x];
 		scrn[y * scrn_buff->size_line / (scrn_buff->bpp / 8) + ray_vars.scrn_x] = pixel;
+		++y;
 	}
+}
+
+void	print_ray(t_ray_vars ray_vars)
+{
+	printf("ray x %f\n", ray_vars.ray_dir.x);
+	printf("ray y %f\n", ray_vars.ray_dir.y);
+	printf("dx %f\n", ray_vars.delta_x);
+	printf("dy %f\n", ray_vars.delta_y);
+	printf("sx %f\n", ray_vars.side_x);
+	printf("sy %f\n", ray_vars.side_y);
 }
 
 void	perform_raycast(t_cache tex_cache, t_player p,
@@ -154,10 +173,11 @@ void	perform_raycast(t_cache tex_cache, t_player p,
 {
 	double		cam_plane_pos_x;
 	t_ray_vars	ray_vars;
-	int			side;
+	int			texture;
 
-	ray_vars.scrn_x = -1;
-	while (++ray_vars.scrn_x < WIN_WIDTH)
+	cam_plane_pos_x = 0.0f;
+	ray_vars = (t_ray_vars){};
+	while (ray_vars.scrn_x < WIN_WIDTH)
 	{
 		cam_plane_pos_x = 2 * ray_vars.scrn_x / (double)WIN_WIDTH - 1;
 		ray_vars.ray_dir = add_vectors(p.dir_vect,
@@ -167,8 +187,8 @@ void	perform_raycast(t_cache tex_cache, t_player p,
 		calc_delta(&ray_vars);
 		calc_side_dist(&ray_vars, p);
 		perform_dda(&ray_vars, map);
-		draw_textured_line(ray_vars, p, tex_cache.textures[ray_vars.side], scrn_buff);
-		// t_line_vars line_vars = calc_line_vals(ray_vars);
-		// draw_ver_line(scrn_buff, ray_vars.scrn_x, line_vars.line_start, line_vars.line_end, GREEN);
+		texture = determine_side_hit(ray_vars);
+		draw_textured_line(ray_vars, p, tex_cache.textures[texture], scrn_buff);
+		++ray_vars.scrn_x;
 	}
 }
